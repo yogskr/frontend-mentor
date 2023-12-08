d4b2b43b6a3f20a874bcbf4c01ddde83ae28dc05
yogskr <yogakrisanta@gmail.com>
2023-12-08 20:38:51 +0700

    Add a warning custom message

--- 5-newsletter-sign-up/assets/js/main.js
+++ 5-newsletter-sign-up/assets/js/main.js
@@ -1,15 +1,23 @@
 // Email validation
 const emailInput = document.getElementById('email-input');
 const errorMsg = document.getElementById('error-msg');
+const warningMsg = document.getElementById('warning-msg');
 const submitBtn = document.getElementById('submit-btn');
 const modalSuccessMsg = document.getElementById('success-msg');
 const successEmailText = document.getElementById('email-address');
 const headerEl = document.getElementById('header');
 const mainEl = document.getElementById('signup-form');
 
+const clearCustomMsg = () => {
+  warningMsg.style.display = 'none';
+  errorMsg.style.display = 'none';
+  emailInput.classList.toggle('custom-msg');
+  emailInput.value = '';
+};
+
 const validateEmail = () => {
-  let mailFormat =
-    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  const mailFormat =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
   let emailValue = emailInput.value;
 
   if (emailValue.match(mailFormat)) {
@@ -17,27 +25,23 @@ const validateEmail = () => {
     mainEl.classList.toggle('hidden');
     headerEl.classList.toggle('hidden');
     successEmailText.innerText = emailInput.value;
-  } else if (!emailValue.match(mailFormat)) {
-    errorMsg.style.display = 'block';
-    emailInput.style.border = '1px solid hsl(4, 100%, 67%)';
-    emailInput.style.backgroundColor = 'rgba(255, 87, 135, 0.1)';
-    emailInput.style.color = 'hsl(4, 100%, 67%)';
-
-    const clearErrorMsg = () => {
-      errorMsg.style.display = 'none';
-      emailInput.style.border = '';
-      emailInput.style.backgroundColor = '';
-      emailInput.style.color = '';
-      emailInput.value = '';
-    };
+  } else if (emailValue == '') {
+    errorMsg.style.display = 'none';
+    warningMsg.style.display = 'flex';
+    emailInput.classList.toggle('custom-msg');
 
     setTimeout(() => {
-      clearErrorMsg();
+      clearCustomMsg();
+    }, 2500);
+  } else if (!emailValue.match(mailFormat)) {
+    errorMsg.style.display = 'flex';
+    emailInput.classList.toggle('custom-msg');
+
+    setTimeout(() => {
+      clearCustomMsg();
     }, 2500);
   }
 };
-
-const displayErrorMsg = () => {};
 
 submitBtn.addEventListener('click', validateEmail);
 emailInput.addEventListener('keydown', (e) => {
