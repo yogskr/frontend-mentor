a29d36003456ab20eda60e4609a439d02cc61049
yogskr <yogakrisanta@gmail.com>
2024-03-03 05:51:32 +0700

    Added custom backgroundImages

--- 10-loopstudios-landing-page/tailwind.config.js
+++ 10-loopstudios-landing-page/tailwind.config.js
@@ -9,6 +9,15 @@ export default {
       },
       backgroundImage: {
         heroPattern: "url('src/assets/images/mobile/image-hero.jpg')",
+        deepEarth: "url('/src/assets/images/mobile/image-deep-earth.jpg')",
+        nightArcade: "url('/src/assets/images/mobile/image-night-arcade.jpg')",
+        soccerTeam: "url('/src/assets/images/mobile/image-soccer-team.jpg')",
+        theGrid: "url('/src/assets/images/mobile/image-grid.jpg')",
+        fromAbove: "url('/src/assets/images/mobile/image-from-above.jpg')",
+        pocketBorealis:
+          "url('/src/assets/images/mobile/image-pocket-borealis.jpg')",
+        theCuriosity: "url('/src/assets/images/mobile/image-curiosity.jpg')",
+        fisheye: "url('/src/assets/images/mobile/image-fisheye.jpg')",
       },
       textColor: {
         darkGray: 'hsl(0, 0%, 55%)',
