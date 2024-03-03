5d6f02d3a6677b42bfc732df7f10aea821f15070
yogskr <yogakrisanta@gmail.com>
2024-03-03 16:36:15 +0700

    Created a route to store the Projects section's content

---
+++ 10-loopstudios-landing-page/src/routes/projects-routes.jsx
@@ -0,0 +1,50 @@
+export const projectTexts = [
+  {
+    id: 0,
+    titleOne: "DEEP",
+    titleTwo: "EARTH",
+    background: "bg-deepEarth",
+  },
+  {
+    id: 1,
+    titleOne: "NIGHT",
+    titleTwo: "ARCADE",
+    background: "bg-nightArcade",
+  },
+  {
+    id: 2,
+    titleOne: "SOCCER",
+    titleTwo: "TEAM VR",
+    background: "bg-soccerTeam",
+  },
+  {
+    id: 3,
+    titleOne: "THE",
+    titleTwo: "GRID",
+    background: "bg-theGrid",
+  },
+  {
+    id: 4,
+    titleOne: "FROM UP",
+    titleTwo: "ABOVE VR",
+    background: "bg-fromAbove",
+  },
+  {
+    id: 5,
+    titleOne: "POCKET",
+    titleTwo: "BOREALIS",
+    background: "bg-pocketBorealis",
+  },
+  {
+    id: 6,
+    titleOne: "THE",
+    titleTwo: "CURIOSITY",
+    background: "bg-theCuriosity",
+  },
+  {
+    id: 7,
+    titleOne: "MAKE IT",
+    titleTwo: "FISHEYE",
+    background: "bg-fishEye",
+  },
+];
