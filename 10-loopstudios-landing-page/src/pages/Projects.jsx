b37187e4972c69a188a78891050156e35de04699
yogskr <yogakrisanta@gmail.com>
2024-03-03 16:37:39 +0700

    Added a functionality to map the Projects content

--- 10-loopstudios-landing-page/src/pages/Projects.jsx
+++ 10-loopstudios-landing-page/src/pages/Projects.jsx
@@ -1,15 +1,30 @@
+import { projectTexts } from "../routes/projects-routes";
+
 export default function Projects() {
+  const projectItems = projectTexts.map((text) => (
+    <figure
+      key={text.id}
+      className={`flex h-[125px] w-full ${text.background} bg-contain bg-center bg-no-repeat`}
+    >
+      <span className="flex h-full w-full items-end bg-gradient-to-r from-black/70 p-5 ">
+        <p className="flex w-full flex-col text-balance font-josefinSans text-[1.5rem] font-extralight leading-none text-white">
+          <span>{text.titleOne}</span>
+          <span>{text.titleTwo}</span>
+        </p>
+      </span>
+    </figure>
+  ));
+
   return (
-    <section>
-      <h2 className="text-center font-josefinSans text-[2rem] font-light">
+    <section className="px-5 pb-20 pt-10">
+      <h2 className="mb-10 text-center font-josefinSans text-[2rem] font-light">
         OUR CREATIONS
       </h2>
-      <div>
-        <figure className="flex h-[135px] w-full items-end bg-deepEarth bg-cover bg-no-repeat p-4">
-          <figcaption className="w-1/3 font-josefinSans text-3xl font-light leading-none text-white">
-            DEEP EARTH
-          </figcaption>
-        </figure>
+      <div className="flex flex-col items-center gap-5">
+        {projectItems}
+        <button className=" border border-black bg-white p-[0.6rem] px-12 align-middle font-josefinSans font-semibold tracking-widest">
+          SEE ALL
+        </button>
       </div>
     </section>
   );
