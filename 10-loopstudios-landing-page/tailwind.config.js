d5323f82ccd73965ce6cbc99d81ae11162ef717e
yogskr <yogakrisanta@gmail.com>
2024-03-03 16:35:22 +0700

    Edited the backgroundImage object element text

--- 10-loopstudios-landing-page/tailwind.config.js
+++ 10-loopstudios-landing-page/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        alata: ['Alata', 'sans-serif'],
-        josefinSans: ['Josefin Sans', 'sans-serif'],
+        alata: ["Alata", "sans-serif"],
+        josefinSans: ["Josefin Sans", "sans-serif"],
       },
       backgroundImage: {
         heroPattern: "url('src/assets/images/mobile/image-hero.jpg')",
@@ -17,11 +17,11 @@ export default {
         pocketBorealis:
           "url('/src/assets/images/mobile/image-pocket-borealis.jpg')",
         theCuriosity: "url('/src/assets/images/mobile/image-curiosity.jpg')",
-        fisheye: "url('/src/assets/images/mobile/image-fisheye.jpg')",
+        fishEye: "url('/src/assets/images/mobile/image-fisheye.jpg')",
       },
       textColor: {
-        darkGray: 'hsl(0, 0%, 55%)',
-        veryDarkGray: 'hsl(0, 0%, 41%)',
+        darkGray: "hsl(0, 0%, 55%)",
+        veryDarkGray: "hsl(0, 0%, 41%)",
       },
     },
   },
