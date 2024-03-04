b149dac7d5e8c7db22b2d66d0b51a719be867728
yogskr <yogakrisanta@gmail.com>
2024-03-04 10:11:56 +0700

    Rendered the text content dynamically

--- 10-loopstudios-landing-page/src/pages/About.jsx
+++ 10-loopstudios-landing-page/src/pages/About.jsx
@@ -1,3 +1,6 @@
+import { aboutTexts } from "../assets/routes/text-data";
+import { uppercaseLetter } from "../assets/helpers/helper";
+
 export default function About() {
   return (
     <section className="flex flex-col items-center gap-10 px-5 pb-10 pt-20">
@@ -7,13 +10,10 @@ export default function About() {
       />
       <article className="flex flex-col items-center justify-center text-center ">
         <h2 className="mb-4 font-josefinSans text-[2rem] font-light leading-none">
-          THE LEADER IN INTERACTIVE VR
+          {uppercaseLetter(aboutTexts.heading)}
         </h2>
         <p className="w-72 font-alata leading-loose text-veryDarkGray">
-          Founded in 2011, Loopstudios has been producing world-class virtual
-          reality projects for some of the best companies around the globe. Our
-          award-winning creatins have transformed businesses through digital
-          experiences that bind to their brand.
+          {aboutTexts.paragraph}
         </p>
       </article>
     </section>
