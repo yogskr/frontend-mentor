68727a057dd511d853da7e1f09a6b246596a086e
yogskr <yogakrisanta@gmail.com>
2024-03-04 08:36:52 +0700

    Renamed the file name extension

---
+++ 10-loopstudios-landing-page/src/assets/routes/nav-routes.js
@@ -0,0 +1,22 @@
+export const navTexts = [
+  {
+    id: 0,
+    title: "About",
+  },
+  {
+    id: 1,
+    title: "Careers",
+  },
+  {
+    id: 2,
+    title: "Events",
+  },
+  {
+    id: 3,
+    title: "Products",
+  },
+  {
+    id: 4,
+    title: "Support",
+  },
+];
