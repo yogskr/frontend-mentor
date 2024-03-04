2b19bfc6383246497f47b6c6eb7f8219e0e083e9
yogskr <yogakrisanta@gmail.com>
2024-03-04 10:09:38 +0700

    Changed default export to named export

--- 10-loopstudios-landing-page/src/assets/helpers/helper.js
+++ 10-loopstudios-landing-page/src/assets/helpers/helper.js
@@ -1,3 +1,7 @@
-export default function uppercaseLetter(text) {
+export function uppercaseLetter(text) {
   return text.toUpperCase();
+}
+
+export function capitalizedFirstLetter(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
