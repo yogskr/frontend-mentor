b5c836ea7b831e1e28ab3f9de3de8aa87ff6554f
yogskr <yogakrisanta@gmail.com>
2024-03-04 10:08:47 +0700

    Modified and added text for the page content

---
+++ 10-loopstudios-landing-page/src/assets/routes/text-data.js
@@ -0,0 +1,87 @@
+// Navigation Menu
+export const navTexts = [
+  {
+    id: 0,
+    title: "about",
+  },
+  {
+    id: 1,
+    title: "careers",
+  },
+  {
+    id: 2,
+    title: "events",
+  },
+  {
+    id: 3,
+    title: "products",
+  },
+  {
+    id: 4,
+    title: "support",
+  },
+];
+
+// About
+export const aboutTexts = {
+  heading: "the leader in interactive vr",
+  paragraph:
+    "Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creatins have transformed businesses through digital experiences that bind to their brand.",
+};
+
+// Projects
+export const projectText = {
+  heading: "our creations",
+  button: "see all",
+};
+
+export const projectListTexts = [
+  {
+    id: 0,
+    titleOne: "deep",
+    titleTwo: "earth",
+    background: "bg-deepEarth",
+  },
+  {
+    id: 1,
+    titleOne: "night",
+    titleTwo: "arcade",
+    background: "bg-nightArcade",
+  },
+  {
+    id: 2,
+    titleOne: "soccer",
+    titleTwo: "team vr",
+    background: "bg-soccerTeam",
+  },
+  {
+    id: 3,
+    titleOne: "the",
+    titleTwo: "grid",
+    background: "bg-theGrid",
+  },
+  {
+    id: 4,
+    titleOne: "from up",
+    titleTwo: "above",
+    background: "bg-fromAbove",
+  },
+  {
+    id: 5,
+    titleOne: "pocket",
+    titleTwo: "borealis",
+    background: "bg-pocketBorealis",
+  },
+  {
+    id: 6,
+    titleOne: "the",
+    titleTwo: "curiosity",
+    background: "bg-theCuriosity",
+  },
+  {
+    id: 7,
+    titleOne: "make it",
+    titleTwo: "fish eye",
+    background: "bg-fishEye",
+  },
+];
