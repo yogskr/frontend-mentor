8232af92f1fb604207ee87bd8113ae114dd86b52
yogskr <yogakrisanta@gmail.com>
2024-03-04 10:12:26 +0700

    Changed the section tag to the footer tag

--- 10-loopstudios-landing-page/src/pages/Footer.jsx
+++ 10-loopstudios-landing-page/src/pages/Footer.jsx
@@ -1,3 +1,13 @@
+import { icons } from "../assets/routes/icons";
+import NavMenu from "../components/NavMenu";
+
 export default function Footer() {
-  return <section></section>;
+  return (
+    <footer className="flex flex-col items-center justify-center bg-black py-14">
+      <img className="w-36" src={icons.logo} alt="Loopstudios logo" />
+      <ul className="flex flex-col items-center gap-4 py-10 font-alata text-white">
+        <NavMenu isHeader={false} />
+      </ul>
+    </footer>
+  );
 }
