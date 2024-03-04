34e6dd3c6823bf7cc335cc3dc4db8c45fee99ae0
yogskr <yogakrisanta@gmail.com>
2024-03-04 12:35:50 +0700

    Modified the classes, styles, and added a div tag

--- 10-loopstudios-landing-page/src/pages/Footer.jsx
+++ 10-loopstudios-landing-page/src/pages/Footer.jsx
@@ -1,13 +1,19 @@
-import { icons } from "../assets/routes/icons";
+import { logo } from "../assets/routes/icons";
+import { footerText } from "../assets/routes/text-data";
 import NavMenu from "../components/NavMenu";
+import SocialMedia from "../components/SocialMedia";
 
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center bg-black py-14">
-      <img className="w-36" src={icons.logo} alt="Loopstudios logo" />
-      <ul className="flex flex-col items-center gap-4 py-10 font-alata text-white">
+      <img className="w-36" src={logo.src} alt={logo.alt} />
+      <ul className="flex h-[16rem] flex-col items-center gap-4 pb-0 pt-10 font-alata text-white">
         <NavMenu isHeader={false} />
       </ul>
+      <div className="group flex gap-5">
+        <SocialMedia />
+      </div>
+      <p className="mt-3 font-alata text-darkGray">{footerText.title}</p>
     </footer>
   );
 }
