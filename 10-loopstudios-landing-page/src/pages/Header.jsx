95586472a3d9a290a3250ec47afe0262104052f6
yogskr <yogakrisanta@gmail.com>
2024-03-05 12:29:35 +0700

    Modified the rendered component name

--- 10-loopstudios-landing-page/src/pages/Header.jsx
+++ 10-loopstudios-landing-page/src/pages/Header.jsx
@@ -1,13 +1,13 @@
-import NavMenuMobile from "../components/NavMobile";
+import NavBar from "../components/NavBar";
 import { headerText } from "../assets/routes/text-data";
 import { uppercaseLetter } from "../assets/helpers/helper";
 
 export default function Header() {
   return (
-    <header className="flex h-screen w-fit flex-col bg-heroPattern bg-cover">
-      <NavMenuMobile />
-      <div className="md:scale-1 flex h-full scale-90 flex-col items-center justify-center">
-        <h1 className="border-[3px] p-5 font-josefinSans text-5xl font-[300] leading-none text-white sm:text-7xl md:text-8xl">
+    <header className="bg-heroPatternMobile lg:bg-heroPatternDesktop flex h-screen w-fit flex-col bg-cover lg:h-[650px] lg:w-full">
+      <NavBar />
+      <div className="flex h-full scale-90 flex-col items-center justify-center lg:scale-100">
+        <h1 className="border-[3px] p-5 font-josefinSans text-5xl font-[300] leading-none text-white md:text-8xl lg:w-1/2 lg:text-5xl">
           {uppercaseLetter(headerText.title)}
         </h1>
       </div>
