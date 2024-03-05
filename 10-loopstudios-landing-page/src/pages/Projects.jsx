77a492102b21421dff5836ec13753d44677822ec
yogskr <yogakrisanta@gmail.com>
2024-03-05 12:30:07 +0700

    Modified the background class name for mobile

--- 10-loopstudios-landing-page/src/pages/Projects.jsx
+++ 10-loopstudios-landing-page/src/pages/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
   const projectItems = projectListTexts.map((text) => (
     <figure
       key={text.id}
-      className={`group flex h-[125px] w-full ${text.background} bg-cover bg-center bg-no-repeat`}
+      className={`group flex h-[125px] w-full ${text.backgroundMobile} bg-cover bg-center bg-no-repeat`}
     >
       <span className="flex h-full w-full items-end bg-gradient-to-r from-black/70 p-5 transition delay-200 duration-300 hover:cursor-pointer hover:from-neutral-300/85 hover:to-neutral-300/85">
         <p className="flex w-full flex-col text-balance font-josefinSans text-[1.5rem] font-extralight leading-none text-white group-hover:font-light group-hover:text-black md:text-[1.8rem]">
@@ -18,7 +18,7 @@ export default function Projects() {
   ));
 
   return (
-    <section className="flex flex-col items-center justify-center gap-10 px-5 pb-20 pt-10 md:px-10">
+    <section className="flex flex-col items-center justify-center gap-10 px-5 pb-20 pt-10 md:px-10 md:pb-28 md:pt-16">
       <h2 className="text-center font-josefinSans text-[2rem] font-light md:text-[2.5rem]">
         {uppercaseLetter(projectText.heading)}
       </h2>
