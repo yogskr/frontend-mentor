b286c2769e02d6521e86d230302f3d451ac000c4
yogskr <yogakrisanta@gmail.com>
2024-03-05 11:06:51 +0700

    Added styles for the 758px screen

--- 10-loopstudios-landing-page/src/components/NavMobile.jsx
+++ 10-loopstudios-landing-page/src/components/NavMobile.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { Squash as Hamburger } from "hamburger-react";
-import { logo } from "../assets/routes/icons";
+import { logo } from "../assets/routes/images-data";
 import NavMenu from "./NavMenu";
 
 export default function NavMenuMobile() {
@@ -28,20 +28,22 @@ export default function NavMenuMobile() {
   }, [scrolled]);
 
   return (
-    <nav ref={ref} className="lg:hidden">
+    <nav ref={ref}>
       <div
-        className={`fixed left-0 right-0 z-20 flex items-center justify-between px-5 py-8 ${
+        className={`fixed left-0 right-0 z-20 flex items-center justify-between px-5 py-8 md:px-10 ${
           scrolled
             ? "backdrop-blur-lg backdrop-brightness-75"
             : "bg-transparent"
         }`}
       >
-        <img src={logo.src} alt={logo.src} />
-        <Hamburger toggled={isOpen} toggle={setIsOpen} color="#fff" />
+        <img className="md:w-60" src={logo.src} alt={logo.src} />
+        <figure className="lg:hidden">
+          <Hamburger toggled={isOpen} toggle={setIsOpen} color="#fff" />
+        </figure>
       </div>
       {isOpen && (
-        <aside className="fixed bottom-0 right-0 z-10 h-full w-full bg-black px-5 py-8 text-white">
-          <ul className="flex h-full flex-col items-start justify-center gap-6 font-josefinSans text-2xl font-light">
+        <aside className="fixed bottom-0 right-0 z-10 h-full w-full bg-black px-5 py-8 text-white md:px-10">
+          <ul className="flex h-full flex-col items-start justify-center gap-6 font-josefinSans text-2xl font-light md:items-center md:gap-12 md:text-4xl">
             <NavMenu isHeader={true} />
           </ul>
         </aside>
