aa555107f4830d1a49e1ce2f992fa8a492c109f7
yogskr <yogakrisanta@gmail.com>
2024-03-05 12:28:58 +0700

    Modified the img src for mobile

--- 10-loopstudios-landing-page/src/pages/About.jsx
+++ 10-loopstudios-landing-page/src/pages/About.jsx
@@ -4,8 +4,8 @@ import { uppercaseLetter } from "../assets/helpers/helper";
 
 export default function About() {
   return (
-    <section className="flex flex-col items-center gap-10 px-5 pb-10 pt-20 md:px-10">
-      <img src={about.src} alt={about.alt} />
+    <section className="flex flex-col items-center gap-10 px-5 pb-10 pt-20 md:px-10 md:pb-16 md:pt-28">
+      <img src={about.srcMobile} alt={about.alt} />
       <article className="flex flex-col items-center justify-center text-center">
         <h2 className="mb-4 font-josefinSans text-[2rem] font-light leading-none md:text-[2.5rem]">
           {uppercaseLetter(aboutTexts.heading)}
