37bb323034cc76df0dbe0de6f9a8a99a87e9e51d
yogskr <yogakrisanta@gmail.com>
2024-03-06 19:22:08 +0700

    Added styles for the desktop view

--- 10-loopstudios-landing-page/src/pages/Footer.jsx
+++ 10-loopstudios-landing-page/src/pages/Footer.jsx
@@ -5,15 +5,21 @@ import SocialMedia from "../components/SocialMedia";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center bg-black py-14">
-      <img className="w-36 md:w-44" src={logo.src} alt={logo.alt} />
-      <ul className="flex h-[16rem] flex-col items-center gap-4 pb-0 pt-10 font-alata text-white md:text-[1rem]">
-        <NavMenu isHeader={false} />
-      </ul>
-      <div className="group flex gap-5">
-        <SocialMedia />
+    <footer className="flex flex-col items-center justify-center bg-black py-14 lg:flex-row lg:justify-between lg:px-20 xl:px-40">
+      <div>
+        <img className="w-36 md:w-44" src={logo.src} alt={logo.alt} />
+        <ul className="flex h-[16rem] flex-col items-center gap-4 pb-0 pt-10 font-alata text-white md:text-[1rem] lg:mt-4 lg:h-[2rem] lg:flex-row lg:pt-0">
+          <NavMenu isHeader={false} />
+        </ul>
       </div>
-      <p className="mt-3 font-alata text-darkGray">{footerText.title}</p>
+      <div className="flex flex-col items-center justify-center lg:items-end">
+        <ul className="group flex gap-4">
+          <SocialMedia />
+        </ul>
+        <p className="mt-3 font-alata text-darkGray lg:mt-4">
+          {footerText.title}
+        </p>
+      </div>
     </footer>
   );
 }
